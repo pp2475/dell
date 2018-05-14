f1c66f4a66797fe933f7221e1c82f2ab43e1c76a
Peter Xu <17117561@qq.com>
2018-05-14 13:15:34 +0000

    chapter 4.4

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,25 @@
 <html>
 <head>
     <title>@yield('title', 'dell') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-    <h1>@yield('content')</h1>
+    <header class="navbar navbar-fixed-top navbar-inverse">
+        <div class="container">
+            <div class="col-md-offset-1 col-md-10">
+                <a href="/" id="logo">Sample App</a>
+                <nav>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="/help">帮助</a></li>
+                        <li><a href="#">登录</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        @yield('content')
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 @section('title', '主页')
 @section('content')
-<h1>主页</h1>
+<div class="jumbotron">
+    <h1>Hello World</h1>
+    <p class="lead">
+        你现在看到的是 <a href="http://www.ethlover.com">Ethlover</a> 的示例项目
+    </p>
+    <p>
+        一切，从这里开始
+    </p>
+    <p>
+        <a href="#" class="btn btn-lg btn-success" roll="button">现在注册</a>
+    </p>
+</div>
 @stop
