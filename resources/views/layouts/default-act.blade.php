ee998907857420cc3ecfbfba86861f868710f6cf
Peter Xu <17117561@qq.com>
2018-06-08 15:06:59 +0000

    new default layout

---
+++ resources/views/layouts/default-act.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'dell') - Laravel 新手入门教程</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+    <div class="head-banner">
+        @yield('content')
+    </div>
+    <div>
+
+    </div>
+</body>
+</html>
